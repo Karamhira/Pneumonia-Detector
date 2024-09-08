b7137efc5fdef76c680b386e341684edc4b1f4a4
Karamvir <karam.hira10@gmail.com>
2024-09-08 13:20:28 -0400

    Working on front end

---
+++ Model/frontend/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="home">
+      <h1>Pneumonia Detector</h1>
+      <input type="file" @change="onFileChange" />
+      <button @click="uploadImage" :disabled="!file">Upload Image</button>
+      <div v-if="result" class="result">
+        <h2>Diagnosis Result:</h2>
+        <p>{{ result }}</p>
+      </div>
+    </div>
+  </template>
